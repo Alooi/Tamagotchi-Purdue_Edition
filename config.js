95ceaa019ae355ae54faca4efc37e958460080ab
hasanaksay <haksay@purdue.edu>
2019-04-24 01:45:54 -0400

    file config 2

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDqQD1lyxMgTFcKcYi0XtNlMtzGNlLp2zY",
-    authDomain: "tamagotchi-server.firebaseapp.com",
-    databaseURL: "https://tamagotchi-server.firebaseio.com",
-    projectId: "tamagotchi-server",
-    storageBucket: "tamagotchi-server.appspot.com",
-    messagingSenderId: "1096773357408"
-};
-module.exports = config;
