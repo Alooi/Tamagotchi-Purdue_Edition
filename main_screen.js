bbe07371b954b4dc9ee993016f63c1c8083cb70b
Alooi <alnasser@purdue.edu>
2019-04-24 01:12:44 -0400

    server side prompt

--- main_screen.js
+++ main_screen.js
@@ -14,6 +14,7 @@ var collectButton = document.getElementById("feedButton");
 
 // var playereName = prompt("Enter Username", "Username");
 // var playerePass = prompt("Enter Password", "Password");
+var url = prompt("Type server side URL");
 
 var http = new XMLHttpRequest();
 
